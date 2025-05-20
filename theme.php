0bd4229881f359386bd87f0e8bdf430860cb7c78
Yiğit <2310205027@ogrenci.karabuk.edu.tr>
2025-05-20 17:28:55 +0300

    Update theme.php
    ödeve daha uygun hale getirdim

--- theme.php
+++ theme.php
@@ -1,4 +1,6 @@
 <?php
+/* https://www.w3schools.com/php/func_network_setcookie.asp BURADAN YARDIM ALDIM*/
+
 if (isset($_COOKIE['theme']) && $_COOKIE['theme'] === 'dark') {
   setcookie('theme', 'light', time() + (86400 * 30), "/");
 } else {
